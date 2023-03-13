3159f0af4c443b57e022f17051164133cb0b1f72
amalnathsathyan <amalnath.sn@pitsolutions.com>
2023-03-13 21:59:23 +0530

    cleared webpack errors, somethig error with id

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+const path = require('path');
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["raw.githubusercontent.com"],
+    domains: ['raw.githubusercontent.com'],
+  },
+  webpack: (config, { isServer }) => {
+    // Add custom webpack configurations here
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
+    config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+    return config;
   },
 };
-
-module.exports = nextConfig;
