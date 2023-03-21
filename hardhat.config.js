5a07f7e71ea07fbef9e74d48a411993100c409b4
amalnathsathyan <amalnath.sn@pitsolutions.com>
2023-03-21 12:49:34 +0530

    nothing to compile

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+//hardhat config file
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
   networks: {
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
+      url: 'https://rpc-mumbai.maticvigil.com',
       accounts: [process.env.PRIVATE_KEY],
     },
   },
   paths: {
-    artifacts: "./artifacts",
+    artifacts: './artifacts',
   },
 };
